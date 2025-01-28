f0c9841ead0d20a3a55b71a74e64f6ac23aacccd
Nick roy <nroy08232@gmail.com>
2025-01-28 18:02:17 +0600

    2nd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,3 +80,19 @@ document.addEventListener('DOMContentLoaded', () => {
     
   });
       //---- Slider End
+      // Get the button
+let backToTopBtn = document.getElementById('backToTopBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
